48b6b058f4379ea2752e6fc612760195f6d233c3
Ira Jane Renoblas <irajanerenoblas067@gmail.com>
2023-11-18 13:35:15 +0800

    Initial Change

--- App.js
+++ App.js
@@ -1,10 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Modal, Alert } from 'react-native';
+import { ScrollView, TouchableOpacity } from 'react-native-web';
+import guest from './src/Guests';
+import React, {useState} from 'react';
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null);
+
+  const show = (guestss) =>{
+    setModalVisible(true);
+    setSelectedUser(guestss);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+    <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false);
+        }}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+                {selectedUser &&(
+                  <Text style={{fontSize: 28, fontWeight: '600',  fontFamily: 'Tahoma', padding: 15}}>Hello, {selectedUser.nick}!</Text>
+                )}
+                {selectedUser && (
+                  <Text style={{fontSize: 28,  fontFamily: 'Tahoma', color: '#0802A3', fontWeight: '100'}}>{selectedUser.name.fname} {selectedUser.name.lname},
+                  </Text>
+                )}
+                {selectedUser && (
+                  <Text style={{fontWeight: '100',  fontFamily: 'Tahoma', color: '#F94C10', fontSize: 20}}>{selectedUser.course}</Text>
+                )}
+
+                <TouchableOpacity onPress={()=> setModalVisible(false)}>
+                  <Text style={{marginTop: 25, fontSize: 20, color: '#776B5D'}}>Close</Text>
+                </TouchableOpacity>
+                       
+            </View>
+          </View>
+        </Modal>
+
+      <Text style={styles.txt}>SCROLLVIEW</Text>
+      {guest.map ((guestss)=>{
+        return(
+          <View>
+            <ScrollView>
+              <TouchableOpacity onPress={()=> show(guestss)} style={styles.border}>
+                <Text style={styles.txt1}>{guestss.nick}</Text>
+              </TouchableOpacity>
+                
+            </ScrollView>
+
+          </View>
+        )
+      })}
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +67,47 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    marginTop: 10,
+  },
+  txt: {
+    fontWeight: '700',
+    fontFamily: 'Tahoma',
+    fontSize: 35,
+    color: '#557C55',
+    marginLeft: 10
+
+  },
+  txt1: {
+    fontWeight: '500',
+    fontFamily: 'Tahoma',
+    fontSize: 25,
+    marginLeft: 15,
+    margin: 10
+  },
+  border: {
+    borderWidth: 0.1,
+    borderColor: '#B2533E'
+
+  },
+  centeredView: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.50,
+    shadowRadius: 4,
+    elevation: 5,
   },
 });
---
+++ src/Guests.js
@@ -0,0 +1,274 @@
+const guest = [
+    {
+      id: 1,
+      name: { 
+        fname: "Nikko Brylle",
+        lname: "Alturas"
+      },
+      nick: "Nikko",
+      course: "BSIT - 3B"
+  },
+  {
+      id: 2,
+      name: { 
+        fname: "Chariz",
+        lname: "Baco"
+      },
+      nick: "Chacha",
+      course: "BSIT - 3B"
+  },
+  {
+    id: 3,
+    name: { 
+      fname: "Analie",
+      lname: "Barbanida"
+    },
+    nick: "Antet",
+    course: "BSIT - 3B"
+},
+  {
+      id: 4,
+      name: { 
+        fname: "Riza",
+        lname: "Batulanon"
+      },
+      nick: "Rizz",
+      course: "BSIT - 3B"
+  },
+  {
+      id: 5,
+      name: { 
+        fname: "Dave Francis",
+        lname: "Baugbog"
+      },
+      nick: "David",
+      course: "BSIT - 3B"
+  },
+  {
+      id: 6,
+      name: { 
+        fname: "Mirafe",
+        lname: "Betasa"
+      },
+      nick: "Mia",
+      course: "BSIT - 3B"
+  },
+  {
+      id: 7,
+      name: { 
+        fname: "Mae Flor",
+        lname: "Butawan"
+      },
+      nick: "Mae Flor",
+      course: "BSIT - 3B"
+  },
+  {
+      id: 8,
+      name: { 
+        fname: "Kimberly",
+        lname: "Comeghod"
+      },
+      nick: "Kim",
+      course: "BSIT - 3B"
+  },
+  {
+    id: 9,
+    name: { 
+      fname: "Jenmarie",
+      lname: "Cumahig"
+    },
+    nick: "Jenjen",
+    course: "BSIT - 3B"
+  },
+  {
+    id: 10,
+    name: { 
+      fname: "Jovert",
+      lname: "Davirao"
+    },
+    nick: "Jovert",
+    course: "BSIT - 3B"
+  },
+  {
+    id: 11,
+    name: { 
+      fname: "Marjoleine",
+      lname: "Egama"
+    },
+    nick: "Marjo",
+    course: "BSIT - 3B"
+  },
+  {
+    id: 12,
+    name: { 
+      fname: "James Anthony",
+      lname: "Estocado"
+    },
+    nick: "Ja-mes",
+    course: "BSIT - 3B"
+  },
+  {
+    id: 13,
+    name: { 
+      fname: "Rea Angela",
+      lname: "Galleros"
+    },
+    nick: "Rea",
+    course: "BSIT - 3B"
+  },
+  {
+    id: 14,
+    name: { 
+      fname: "Rogel",
+      lname: "Garcia"
+    },
+    nick: "Rogel",
+    course: "BSIT - 3B"
+  },
+  {
+    id: 15,
+    name: { 
+      fname: "Junrey",
+      lname: "Genzon"
+    },
+    nick: "Junrey",
+    course: "BSIT - 3B"
+  },
+  {
+    id: 16,
+    name: { 
+      fname: "Peter Kyle",
+      lname: "Gingo"
+    },
+    nick: "Peter",
+    course: "BSIT - 3B"
+},
+{
+    id: 17,
+    name: { 
+      fname: "Danica Mae",
+      lname: "Gonzaga"
+    },
+    nick: "Dandan",
+    course: "BSIT - 3B"
+},
+{
+    id: 18,
+    name: { 
+      fname: "Clea",
+      lname: "Gultiano"
+    },
+    nick: "Clea",
+    course: "BSIT - 3B"
+},
+{
+    id: 19,
+    name: { 
+      fname: "Deserrie",
+      lname: "Gumonit"
+    },
+    nick: "Deserrie",
+    course: "BSIT - 3B"
+},
+{
+    id: 20,
+    name: { 
+      fname: "Mark Aldwin",
+      lname: "Handumon"
+    },
+    nick: "Angkol",
+    course: "BSIT - 3B"
+},
+{
+    id: 21,
+    name: { 
+      fname: "Ma Laiza",
+      lname: "Hangad"
+    },
+    nick: "Laiza",
+    course: "BSIT - 3B"
+},
+{
+    id: 22,
+    name: { 
+      fname: "Marie Therese",
+      lname: "Jaum"
+    },
+    nick: "Marie",
+    course: "BSIT - 3B"
+},
+{
+    id: 23,
+    name: { 
+      fname: "Rodel Glenn",
+      lname: "Jawa"
+    },
+    nick: "Rodel",
+    course: "BSIT - 3B"
+},
+{
+  id: 24,
+  name: { 
+    fname: "Jess Raphael;",
+    lname: "Jumawan"
+  },
+  nick: "Jess",
+  course: "BSIT - 3B"
+},
+{
+  id: 25,
+  name: { 
+    fname: "John Mark",
+    lname: "Lagrimas"
+  },
+  nick: "JM",
+  course: "BSIT - 3B"
+},
+{
+  id: 26,
+  name: { 
+    fname: "Pearl Angelie",
+    lname: "Lansang"
+  },
+  nick: "Pearl",
+  course: "BSIT - 3B"
+},
+{
+  id: 27,
+  name: { 
+    fname: "Monaliza",
+    lname: "Liparto"
+  },
+  nick: "Moning",
+  course: "BSIT - 3B"
+},
+{
+  id: 28,
+  name: { 
+    fname: "Karl",
+    lname: "Loquias"
+  },
+  nick: "Karl",
+  course: "BSIT - 3B"
+},
+{
+  id: 29,
+  name: { 
+    fname: "Jude Andrey",
+    lname: "Lumayag"
+  },
+  nick: "Jude",
+  course: "BSIT - 3B"
+},
+{
+  id: 30,
+  name: { 
+    fname: "James Raymond",
+    lname: "Noble"
+  },
+  nick: "James",
+  course: "BSIT - 3B"
+},
+  ];
+
+  export default guest;
